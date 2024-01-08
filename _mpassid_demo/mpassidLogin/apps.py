42cc79fac7bd75b25c2cce519a674ab5ef18ee5f
RPLaine <riku.laine@gmail.com>
2024-01-08 10:29:55 +0200

    Initial settings for Django

---
+++ _mpassid_demo/mpassidLogin/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MpassidLoginConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'mpassidlogin'
---
+++ _mpassid_demo/mpassidUserpage/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MpassiduserpageConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'mpassidUserpage'
