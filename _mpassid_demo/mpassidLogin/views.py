3024aee20072bee4531b171bf4f48c31260546a2
RPLaine <riku.laine@gmail.com>
2024-01-08 15:23:16 +0200

    Preparing environmental variables

--- _mpassid_demo/mpassidLogin/views.py
+++ _mpassid_demo/mpassidLogin/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+import os
 
 def index(request):
     return render(request, 'mpassidLogin/index.html')
+
+def redirect(request):
+    return render(request, os.getenv('REDIRECT_URL'))
