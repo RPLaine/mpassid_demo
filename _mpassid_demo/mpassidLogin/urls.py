6e09125c7abfdb316e94c0b605c8256b3a0f4ae8
RPLaine <riku.laine@gmail.com>
2024-01-10 10:48:48 +0200

    Update urls.py
    Removed test function

--- _mpassid_demo/mpassidLogin/urls.py
+++ _mpassid_demo/mpassidLogin/urls.py
@@ -4,5 +4,4 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('redirect/', views.redirect, name='redirect'),
-    path('test/', views.test, name='test'),
 ]
