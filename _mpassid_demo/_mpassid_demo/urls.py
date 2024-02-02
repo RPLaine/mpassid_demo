452d2acec550f40879e2018118b823fb3cd9f029
RPLaine <riku.laine@gmail.com>
2024-02-02 13:39:21 +0200

    Update urls.py

--- _mpassid_demo/_mpassid_demo/urls.py
+++ _mpassid_demo/_mpassid_demo/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(('mpassidLogin.urls', 'mpassidLogin'), namespace='mpassidLogin')),
     path('oauth2/mpassid/', include(('mpassidLogin.urls', 'mpassidLogin'), namespace='mpassidLogin')),
 ]
